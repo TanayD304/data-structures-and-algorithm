c2c43341567c6662e634f82d41e0a4a824a3a2bf
TanayD304 <tdubey304@gmail.com>
2024-12-30 11:44:22 +0530

    Basic Datatypes in Cpp

---
+++ datatypes.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int numInt = INT_MIN;
+    long numLong = LONG_MAX;
+    long long numLongLong = LONG_MIN;
+
+    float numFloat = 10.234;
+    double numDouble = 12.34567;
+
+    char character = 'a';
+    string name = "Tanay";
+
+    bool flag = true;
+
+    cout<<numInt<<endl;
+    cout<<numLong<<endl;
+    cout<<numLongLong<<endl;
+    cout<<numFloat<<endl;
+    cout<<numDouble<<endl;
+    cout<<character<<endl;
+    cout<<name<<endl;
+    cout<<flag<<endl;
+
+
+    return 0;
+
+}
