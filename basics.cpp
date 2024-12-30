2faedc985bf47e4f55da26f800cf61b9a4545204
TanayD304 <tdubey304@gmail.com>
2024-12-30 11:26:34 +0530

    Basic Skeleton of Cpp programs

---
+++ basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>      // Library for including functions for input and output
+#include <bits/stdc++.h> // Library that includes all library
+// Pros - No need to check anything about library
+// Cons - Compilation time is more as all library are complied
+
+using namespace std; // namespace are used to group certain functions and literals together
+// std is one such namespace
+
+int main()
+{   // main function
+    // complier entry point
+    // int is the return type
+    std::cout << "Hello World" << endl;
+    cout << "Tanay Dubey" << endl;
+    return 0; // every function except void has some return value
+    // 0 is returned
+}
