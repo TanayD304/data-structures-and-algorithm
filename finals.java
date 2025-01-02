9f94c7b798fe1d6c57f45fa802e4720f9131f404
TanayD304 <tdubey304@gmail.com>
2025-01-02 15:24:31 +0530

    Basics of Final Keyword in Java

---
+++ finals.java
@@ -0,0 +1,8 @@
+class finals {
+    public static void main(String[] args) {
+        final int num = 10;
+        System.out.println(num);
+        // num=14;
+        // System.out.println(num);
+    }
+}
