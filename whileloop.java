7b1907792d490587e26e20f302e040e4a9b714b3
TanayD304 <tdubey304@gmail.com>
2025-01-02 17:57:42 +0530

    Basic While loop in Java

---
+++ whileloop.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+class whileloop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        while(n>0) {
+            System.out.println("Tanay");
+            n--;
+        }
+    }
+}
