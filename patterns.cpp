d431d081a869e8ecb1c170ed1f2b193db7f27dca
TanayD304 <tdubey304@gmail.com>
2025-01-06 21:33:30 +0530

    Hard Problems

--- patterns.cpp
+++ patterns.cpp
@@ -188,6 +188,213 @@ void pattern11(int n) {
     }
 }
 
+void pattern12(int n) {
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=i;j++) {
+            cout<<j;
+        }
+        for(int j=1;j<=2*n-2*i;j++) {
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--) {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern13(int n) {
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<num << " ";
+            num++;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern14(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<(char)(64 + j);
+        }
+        cout<<endl;
+    }
+}
+
+void pattern15(int n) {
+    while (n > 0)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << (char)(64 + j);
+        }
+        cout << endl;
+        n--;
+    }
+}
+
+void pattern16(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << (char)(64 + i);
+        }
+        cout << endl;
+    }
+}
+
+void pattern17(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout<< " ";
+        }
+        int k = 1;
+        while (k <= i)
+        {
+            cout << (char)(64 + k);
+            k++;
+        }
+        k = i - 1;
+        while (k >= 1)
+        {
+            cout << (char)(64 + k);
+            k--;
+        }
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern18(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << (char)(64 + n-i+j)<<" ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern19(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout<<"*";
+        }
+        for (int j = 1; j < i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = n; j >= i; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern20(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+            continue;
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout<<"*";
+        }
+        for (int j = 1; j < i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern21(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == 1 || i == n || j == 1 || j == n)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void pattern22(int n) {
+    int top, left, right, bottom;
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            top = i;
+            left = j;
+            right = 2 * n - 2 - j;
+            bottom = 2 * n - 2 - i;
+            cout << (n - min(min(top, bottom), min(right, left)));
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
     int n;
@@ -203,5 +410,16 @@ int main()
     pattern9(n);
     pattern10(n);
     pattern11(n);
+    pattern12(n);
+    pattern13(n);
+    pattern14(n);
+    pattern15(n);
+    pattern16(n);
+    pattern17(n);
+    pattern18(n);
+    pattern19(n);
+    pattern20(n);
+    pattern21(n);
+    pattern22(n);
     return 0;
 }
--- patterns.java
+++ patterns.java
@@ -1,4 +1,5 @@
 
+import java.lang.Math;
 import java.util.Scanner;
 
 class patterns {
@@ -146,6 +147,173 @@ class patterns {
         }
     }
 
+    public static void pattern12(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= 2 * n - 2 * i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern13(int n) {
+        int num = 1;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(num+ " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern14(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char)(64+j));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern15(int n) {
+        while (n > 0) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((char) (64 + j));
+            }
+            System.out.println();
+            n--;
+        }
+    }
+
+    public static void pattern16(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (64 + i));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern17(int n) {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n-i;j++) {
+                System.out.print(" ");
+            }
+            int k = 1;
+            while(k<=i) {
+                System.out.print((char) (64 + k));
+                k++;
+            }
+            k=i-1;
+            while(k>=1) {
+                System.out.print((char) (64 + k));
+                k--;
+            }
+            for(int j=1;j<=n-i;j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern18(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char) (64 + n-i+j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern19(int n) {
+        for(int i=1;i<=n;i++) {
+            for(int j=n;j>=i;j--) {
+                System.out.print("*");
+            }
+            for(int j=1;j<i;j++) {
+                System.out.print("  ");
+            }
+            for (int j = n; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            for(int j=1;j<=n-i;j++) {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern20(int n) {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            for(int j=1;j<=n-i;j++) {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++) {
+            if(i==1) continue;
+            for(int j=1;j<=n-i+1;j++) {
+                System.out.print("*");
+            }
+            for(int j=1;j<i;j++) {
+                System.out.print("  ");
+            }
+            for(int j=1;j<=n-i+1;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern21(int n) {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(i==1 || i==n || j==1 || j==n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern22(int n) {
+        int top, left, right, bottom;
+        for(int i=0;i<2*n-1;i++) {
+            for(int j=0;j<2*n-1;j++) {
+                top = i;
+                left = j;
+                right = 2*n-2-j;
+                bottom = 2*n-2-i;
+                System.out.print(n - Math.min(Math.min(top, bottom), Math.min(right, left)) + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -160,5 +328,16 @@ class patterns {
         pattern9(n);
         pattern10(n);
         pattern11(n);
+        pattern12(n);
+        pattern13(n);
+        pattern14(n);
+        pattern15(n);
+        pattern16(n);
+        pattern17(n);
+        pattern18(n);
+        pattern19(n);
+        pattern20(n);
+        pattern21(n);
+        pattern22(n);
     }
 }
