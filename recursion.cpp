0a7a712dba5519d5a9d5fdeef49cc6294cdd1e86
TanayD304 <tdubey304@gmail.com>
2025-01-07 14:10:41 +0530

    Basic Recursion Theory and Concepts

--- recursion.cpp
+++ recursion.cpp
@@ -2,7 +2,89 @@
 
 using namespace std;
 
+void printXNTimes(int x, int n) {
+    // base
+    if(n==0) return;
+
+    // recursive
+    cout<<x<<" ";
+    printXNTimes(x, n-1);
+}
+
+void print1toN(int n) {
+    // base
+    if(n==0) return;
+
+    // recursive
+    print1toN(n-1);
+    cout<<n<<endl;
+}
+
+void printNto1(int n) {
+    // base
+    if(n==0) return;
+
+    // recursive
+    cout<<n<<endl;
+    printNto1(n-1);
+}
+
+// long long int factorial(int n)
+// {
+//     // your code goes here
+// }
+
+// int NnumbersSum(int N)
+// {
+//     // your code goes here
+// }
+
+// int arraySum(vector<int> &nums)
+// {
+//     // your code goes here
+// }
+
+// vector<char> reverseString(vector<char> &s)
+// {
+//     // your code goes here
+// }
+
+// bool palindromeCheck(string &s)
+// {
+//     // your code goes here
+// }
+
+// bool checkPrime(int num)
+// {
+//     // your code goes here
+// }
+
+// vector<int> reverseArray(vector<int> &nums)
+// {
+//     // your code goes here
+// }
+
+// bool isSorted(vector<int> &nums)
+// {
+//     // your code goes here
+// }
+
+// int addDigits(int num)
+// {
+//     // your code goes here
+// }
+
+// int fib(int n)
+// {
+//     // your code goes here
+// }
+
 int main()
 {
+    int x, n;
+    cin>>x>>n;
+    // printXNTimes(x, n);
+    // print1toN(n);
+    printNto1(n);
     return 0;
 }
--- recursion.java
+++ recursion.java
@@ -1,6 +1,84 @@
+import java.util.Scanner;
+
 class recursion {
 
-    public static void main(String[] args) {
+    public static void printXNTimes(int x, int n) {
+        // base
+        if(n==0) return;
 
+        // recursive
+        System.out.print(x + " ");
+        printXNTimes(x, n-1);
+    }
+
+    public static void print1toN(int n) {
+        // base
+        if (n == 0) {
+            return;
+        }
+
+        // recursive
+        print1toN(n - 1);
+        System.out.println(n);
+    }
+
+    public static void printNto1(int n) {
+        // base
+        if (n == 0) {
+            return;
+        }
+
+        // recursive
+        System.out.println(n);
+        printNto1(n - 1);
+    }
+
+    // public static long factorial(int n) {
+    //     //your code goes here
+    // }
+
+    // public static int NnumbersSum(int N) {
+    //     //your code goes here
+    // }
+
+    // public static int arraySum(int[] nums) {
+    //     //your code goes here
+    // }
+
+    // public static Vector<Character> reverseString(Vector<Character> s) {
+    //     //your code goes here
+    // }
+
+    // public static boolean palindromeCheck(String s) {
+    //     //your code goes here
+    // }
+
+    // public static boolean checkPrime(int num) {
+    //     //your code goes here
+    // }
+
+    // public static int[] reverseArray(int[] nums) {
+    //     //your code goes here
+    // }
+
+    // public static boolean isSorted(ArrayList<Integer> nums) {
+    //     //your code goes here
+    // }
+
+    // public static int addDigits(int num) {
+    //     //your code goes here
+    // }
+
+    // public static int fib(int n) {
+    //     //your code goes here
+    // }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int n = scanner.nextInt();
+        // printXNTimes(x, n);
+        // print1toN(n);
+        printNto1(n);
     }
 }
