0b33bc2676908e6df16e610d814447d772fafee1
TanayD304 <tdubey304@gmail.com>
2025-01-09 21:17:20 +0530

    Files Init

---
+++ fundamentalOfArrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class fundamentalOfArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for(int i=0;i<n;i++) {
+            nums[i] = scanner.nextInt();
+        }
+    }
+}
