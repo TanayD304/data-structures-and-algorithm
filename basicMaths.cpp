23d7d2c555a541d0da6d8d534d2abbaed6017e88
TanayD304 <tdubey304@gmail.com>
2025-01-11 23:57:58 +0530

    Count odd digits in Number Done

--- basicMaths.cpp
+++ basicMaths.cpp
@@ -15,11 +15,24 @@ int countDigit(int n)
     return ans;
 }
 
+int countOddDigit(int n)
+{
+    int ans = 0;
+    while (n > 0)
+    {
+        if (n % 10 % 2 == 1)
+            ans++;
+        n /= 10;
+    }
+    return ans;
+}
+
 int main()
 {
     int n;
     cin>>n;
 
-    cout<<countDigit(n);
+    // cout<<countDigit(n);
+    cout<<countOddDigit(n);
     return 0;
 }
--- basicMaths.java
+++ basicMaths.java
@@ -12,10 +12,20 @@ class basicMaths {
         return ans;
     }
 
+    public static int countOddDigit(int n) {
+        int ans = 0;
+        while(n>0) {
+            if(n%10%2==1) ans++;
+            n/=10;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        System.out.println(countDigit(n));
+        // System.out.println(countDigit(n));
+        System.out.println(countOddDigit(n));
     }
 }
