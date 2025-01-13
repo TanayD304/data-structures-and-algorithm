ce9a9e73b7b35bc99c68289b50f99836917fdb2b
TanayD304 <tdubey304@gmail.com>
2025-01-13 16:09:11 +0530

    Left Rotate array by 1 - Done

--- fundamentalOfArrays.cpp
+++ fundamentalOfArrays.cpp
@@ -1,27 +1,62 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int linearSearch(vector<int> &nums, int target)
-{
+void printArray(vector<int> &nums) {
     for(int i=0;i<nums.size();i++) {
-        if(nums[i]==target) return i;
+        cout<<nums[i]<<" ";
     }
-    return -1;
+    cout<<endl;
 }
 
-int main() {
-    int n;
-    cin>>n;
-    vector<int> nums(n);
-    for(int i=0;i<n;i++) {
-        cin>>nums[i];
+// int linearSearch(vector<int> &nums, int target)
+// {
+//     // your code goes here
+// }
+
+// int largestElement(vector<int> &nums)
+// {
+// }
+
+// int secondLargestElement(vector<int> &nums)
+// {
+//     // your code goes here
+// }
+
+// int findMaxConsecutiveOnes(vector<int> &nums)
+// {
+// }
+
+void rotateArrayByOne(vector<int> &nums)
+{
+    int size = nums.size();
+    int temp = nums[0];
+
+    for(int i=0;i<size-1;i++) {
+        nums[i] = nums[i+1];
     }
 
-    // linear search
-    int target;
-    cin>>target;
-    cout<<linearSearch(nums, target);
+    nums[size-1] = temp;
+}
+
+// void rotateArray(vector<int> &nums, int k)
+// {
+// }
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    // Rotate array to left by 1
+    printArray(nums);
+    rotateArrayByOne(nums);
+    printArray(nums);
 
     return 0;
 }
--- fundamentalOfArrays.java
+++ fundamentalOfArrays.java
@@ -1,13 +1,43 @@
 import java.util.*;
 
 class fundamentalOfArrays {
-
-    public static int linearSearch(int nums[], int target) {
+    public static void printArray(int[] nums) {
         for(int i=0;i<nums.length;i++) {
-            if(nums[i]==target) return i;
+            System.out.print(nums[i]+" ");
         }
-        return -1;
+        System.out.println();
     }
+
+    // public int linearSearch(int nums[], int target) {
+    //     //Your code goes here
+    // }
+
+    // public int largestElement(int[] nums) {
+
+    // }
+
+    // public int secondLargestElement(int[] nums) {
+
+    // }
+
+    // public int findMaxConsecutiveOnes(int[] nums) {
+
+    // }
+
+    public static void rotateArrayByOne(int[] nums) {
+        int size = nums.length;
+
+        int temp = nums[0];
+        for(int i=0;i<size-1;i++) {
+            nums[i]=nums[i+1];
+        }
+        nums[size-1]=temp;
+    }
+
+    // public void rotateArray(int[] nums, int k) {
+
+    // }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -17,8 +47,9 @@ class fundamentalOfArrays {
             nums[i] = scanner.nextInt();
         }
 
-        // linear search
-        int target = scanner.nextInt();
-        System.out.println(linearSearch(nums, target));
+        printArray(nums);
+        rotateArrayByOne(nums);
+        printArray(nums);
+
     }
 }
