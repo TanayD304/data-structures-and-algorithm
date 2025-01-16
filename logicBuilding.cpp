1440665d9334324f1904b00b37fab25844b4bcc6
TanayD304 <tdubey304@gmail.com>
2025-01-16 11:36:19 +0530

    Remove Duplicates from Sorted Array

--- logicBuilding.cpp
+++ logicBuilding.cpp
@@ -1,7 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
+void printArray(vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+}
+
+int removeDuplicates(vector<int> &nums)
+{
+    int size = nums.size();
+
+    int lastUniqueIndex = 0;
+    int indexToInsert = 1;
+    for (int i = 1; i < size; i++)
+    {
+        if (nums[i] != nums[lastUniqueIndex])
+        {
+            nums[indexToInsert++] = nums[i];
+            lastUniqueIndex = i;
+        }
+    }
+
+    return indexToInsert;
+}
+
+int main()
+{
+    vector<int> nums = {-2, 2, 4, 4, 4, 4, 5, 5};
+
+    // Remove Duplicates from sorted Array
+    printArray(nums);
+    cout<<removeDuplicates(nums)<<endl;
+    printArray(nums);
     return 0;
 }
--- logicBuilding.java
+++ logicBuilding.java
@@ -1,5 +1,31 @@
 class logicBuilding {
+    public static void printArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int size = nums.length;
+
+        int lastUniqueIndex = 0;
+        int indexToInsert = 1;
+        for(int i=1;i<size;i++) {
+            if(nums[i]!=nums[lastUniqueIndex]) {
+                nums[indexToInsert++] = nums[i];
+                lastUniqueIndex = i;
+            }
+        }
+
+        return indexToInsert;
+    }
     public static void main(String[] args) {
-        
+        int[] nums = {-2,2,4,4,4,4,5,5};
+
+        // Remove Duplicates from sorted Array
+        printArray(nums);
+        System.out.println(removeDuplicates(nums));
+        printArray(nums);
     }
 }
