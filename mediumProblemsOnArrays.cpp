15d7e5978750e55a90cfa36d8ad36537897d4295
TanayD304 <tdubey304@gmail.com>
2025-01-20 16:40:33 +0530

    Rearrange Array elements by Sign

--- mediumProblemsOnArrays.cpp
+++ mediumProblemsOnArrays.cpp
@@ -39,13 +39,34 @@ public:
 
         return ans;
     }
+
+    vector<int> rearrangeArray(vector<int> &nums)
+    {
+        vector<int> ans(nums.size(), 0);
+        int pos = 0;
+        int neg = 1;
+
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i]>0) {
+                ans[pos] = nums[i];
+                pos+=2;
+            } else if(nums[i]<0) {
+                ans[neg] = nums[i];
+                neg+=2;
+            }
+        }
+
+        return ans;
+    }
 };
 
 int main()
 {
     vector<int> nums1 = {-3, 4, 5, 1, -30, -10};
     Solution sol;
-    vector<int> ans = sol.leaders(nums1);
+    // vector<int> ans = sol.leaders(nums1);
+    // sol.printArray(ans);
+    vector<int> ans = sol.rearrangeArray(nums1);
     sol.printArray(ans);
     return 0;
 }
--- mediumProblemsOnArrays.java
+++ mediumProblemsOnArrays.java
@@ -7,6 +7,13 @@ class Solution {
         System.out.println();
     }
 
+    public void printArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+
     public ArrayList<Integer> leaders(int[] nums) {
         ArrayList<Integer> ans = new ArrayList<>();
 
@@ -20,6 +27,25 @@ class Solution {
         Collections.reverse(ans);
         return ans;
     }
+
+    public int[] rearrangeArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        int pos = 0;
+        int neg = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                ans[pos] = nums[i];
+                pos += 2;
+            } else if (nums[i] < 0) {
+                ans[neg] = nums[i];
+                neg += 2;
+            }
+        }
+
+        return ans;
+    }
 }
 
 class mediumProblemsOnArrays {
@@ -28,7 +54,9 @@ class mediumProblemsOnArrays {
         int[] nums1 = {-3, 4, 5, 1, -30, -10};
 
         Solution sol = new Solution();
-        ArrayList<Integer> ans = sol.leaders(nums1);
-        sol.printList(ans);
+        // ArrayList<Integer> ans = sol.leaders(nums1);
+        // sol.printList(ans);
+        int[] ans = sol.rearrangeArray(nums1);
+        sol.printArray(ans);
     }
 }
